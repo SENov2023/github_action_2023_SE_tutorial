b1ee468cf0b605a32b70e8820e067df72e63c619
Haya Samaana <hayasam@najah.edu>
2026-09-24 11:08:56 +0300

    Clean up LibraryApp.java by removing blank lines
    Removed unnecessary blank lines in LibraryApp.java

--- src/main/java/dtu/library/app/LibraryApp.java
+++ src/main/java/dtu/library/app/LibraryApp.java
@@ -4,14 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
-
-
-
-
-
-
-
 /**
  * This class represents the business logic associated with the library application.
  * @author 
